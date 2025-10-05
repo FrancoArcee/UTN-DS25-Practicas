4f42c693a19c27b163a4d1779adcf039ad5f741d
Franco Arce <165492758+FrancoArcee@users.noreply.github.com>
2025-10-04 23:17:51 -0300

    Corrijo error de cors

--- Libreria/backend/src/app.ts
+++ Libreria/backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { authRoutes } from './routes/auth.routes'
 import { bookRoutes } from './routes/book.routes';
 import { userRoutes } from './routes/user.routes';
 import { handleError } from './middlewares/error.middleware';
@@ -9,12 +10,13 @@ import { logRequest } from './middlewares/logger.middleware';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
 const corsOptions = {
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
@@ -22,6 +24,7 @@ app.use(express.json());
 app.use(logRequest);
 
 //Routes
+app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 app.use('/api/users', userRoutes);
 
