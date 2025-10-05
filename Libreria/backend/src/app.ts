201755d53e78ee9499c5bb3a68e171d3a6966564
Franco Arce <165492758+FrancoArcee@users.noreply.github.com>
2025-10-04 21:10:47 -0300

    Implemento cors

--- Libreria/backend/src/app.ts
+++ Libreria/backend/src/app.ts
@@ -1,16 +1,32 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import { bookRoutes } from './routes/book.routes';
+import { userRoutes } from './routes/user.routes';
 import { handleError } from './middlewares/error.middleware';
 import { logRequest } from './middlewares/logger.middleware';
-import cors from "cors";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(logRequest);
 
+//Routes
 app.use('/api/books', bookRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(handleError);
-app.listen(3000, () => { console.log(`Server corriendo en el puerto 3000`); });
+
+app.listen(PORT, () => { 
+  console.log(`Server corriendo en el puerto ${PORT}`); 
+});
